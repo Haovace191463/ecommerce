fb3b43a51c4202216b3650ef205a2ee99a1a5161
AnhHao555 <hao.ce191463@gmail.com>
2026-05-15 11:31:40 +0700

    N1-T08 Add Swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -3,14 +3,27 @@ import { AppModule } from './app.module';
 import { TransformInterceptor } from './common/interceptors/transform.interceptor';
 import { HttpExceptionFilter } from './common/filters/http-exception.filter';
 import { ValidationPipe } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
  async function bootstrap() {
+  
   const app = await NestFactory.create(AppModule);
+
+  const config = new DocumentBuilder()
+    .setTitle('Ecommerce API')
+    .setDescription('API documentation for Ecommerce project')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   app.useGlobalPipes(new ValidationPipe({
     whitelist: true,
       forbidNonWhitelisted: true,
       transform: true,
   }));
+
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters( new HttpExceptionFilter());
   await app.listen(process.env.PORT ?? 3000);
